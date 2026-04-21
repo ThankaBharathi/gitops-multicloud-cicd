122812dadde3cbdb780e72f3bd21f2eae4a0f5d8
Thanka Bharathi <thankabharathi0@gmail.com>
2026-04-21 20:30:07 +0530

    Added CI pipeline

---
+++ app/go.mod
@@ -0,0 +1,3 @@
+module github.com/ThankaBharathi/gitops-multicloud-cicd
+
+go 1.26.2
---
+++ app/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "🚀 GitOps CI/CD Pipeline is Running!")
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "OK")
+}
+
+func main() {
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/health", healthHandler)
+
+	fmt.Println("Server running on port 8080...")
+	http.ListenAndServe(":8080", nil)
+}
