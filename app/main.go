808ef6d5b93075c9f57b6b6b9b5280b384105c4e
Thanka Bharathi <thankabharathi0@gmail.com>
2026-04-22 20:49:36 +0530

    added k8s manifests

--- app/main.go
+++ app/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "🚀 Thanka Bharathi Microservices !...")
+	fmt.Fprintf(w, "🚀 Thanka's Microservice platform !...")
 }
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
