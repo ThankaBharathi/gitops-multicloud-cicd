728106d5716d9464fe118823fbdf87e88da24e38
Thanka Bharathi <thankabharathi0@gmail.com>
2026-04-22 20:22:38 +0530

    added k8s manifests

--- app/main.go
+++ app/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "🚀 GitOps CI/CD Pipeline is Running! Everything is correct aa !..")
+	fmt.Fprintf(w, "🚀 Thanka Bharathi Microservices !..")
 }
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
